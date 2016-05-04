f60ca792c896b88e23508ab821eb4726e3795666
Sergei Korotkov <serega-kkz@Mail.ru>
2016-05-04 06:08:33 +0300

    Added files via upload

--- __init__.py
+++ __init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 bl_info = {"name": "Tools", 
-           "description": "Tools", 
+           "description": "Utilities to work with the engine Panda3D", 
            "author": "serg-kkz", 
-           "version": (0, 1), 
+           "version": (0, 1, 0), 
            "blender": (2, 7, 6),
            "location": "3D view > Hatcher", 
            "warning": "", 
@@ -11,12 +11,14 @@ bl_info = {"name": "Tools",
            "category": "Panda3D"}
            
 import bpy
+import sys
+import os
+import shutil
 
 class CustomRenderEngine(bpy.types.RenderEngine):
     bl_idname = "Panda3D:Hatcher"
     bl_label = "Panda3D:Hatcher"
     bl_use_preview = True
-
 
     def render(self, scene):
         scale = scene.render.resolution_percentage / 100.0
@@ -47,14 +49,42 @@ class CustomRenderEngine(bpy.types.RenderEngine):
         self.end_result(result)
 
 def register():
+
+
+    # Нужно дописать копирования резервной копии properties_world.py, для востановления при отключении плагина
+
+    # Получаем адрес файла исходника блендера properties_world.py
+    #adress_file = os.path.join(os.getcwd(), str(bpy.app.version[0])+'.'+str(bpy.app.version[1]), 'scripts', 'startup',  'bl_ui', 'properties_world.py')   
+
+    # Получаем адрес директории исходника блендера properties_world.py
+    adress_dir = os.path.join(os.getcwd(), str(bpy.app.version[0])+'.'+str(bpy.app.version[1]), 'scripts', 'startup',  'bl_ui')
+    
+    # Получаем адрес файла исходника в папке экспортера, которым подменим  файл блендера properties_world.py
+    adress_file = os.path.join(os.path.dirname(__file__), 'modules', 'properties_world.py')   
+
+    # Копируем файл
+    shutil.copy(adress_file, adress_dir)
+    
+    # Нужно дописать вывод о уведомлении о перезгрузки blendera
+    
+    
+    
+    '''context = bpy.context
+
+    for mod_name in context.user_preferences.addons.keys():
+    
+        mod = sys.modules[mod_name]
+        #print(mod.bl_info.get('name'))'''
+
     bpy.utils.register_class(CustomRenderEngine)
 
     from bl_ui import ( properties_render, properties_material,)
     properties_render.RENDER_PT_render.COMPAT_ENGINES.add(CustomRenderEngine.bl_idname)
     properties_material.MATERIAL_PT_preview.COMPAT_ENGINES.add(CustomRenderEngine.bl_idname)
 
-
 def unregister():
+
+
     bpy.utils.unregister_class(CustomRenderEngine)
 
     from bl_ui import ( properties_render, properties_material,)
