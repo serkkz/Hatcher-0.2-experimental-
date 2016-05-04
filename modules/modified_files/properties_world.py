9ae820dc4523f5c7a16d5cad466381590ad260da
Sergei Korotkov <serega-kkz@Mail.ru>
2016-05-05 02:49:04 +0300

    Added files via upload

---
+++ modules/modified_files/properties_world.py
@@ -0,0 +1,26 @@
+import bpy
+from bpy.types import Panel
+from rna_prop_ui import PropertyPanel
+
+
+class WorldButtonsPanel:
+    bl_space_type = 'PROPERTIES'
+    bl_region_type = 'WINDOW'
+
+    @classmethod
+    def poll(cls, context):
+        return context.world and (context.scene.render.engine in cls.COMPAT_ENGINES)
+
+
+class MATERIAL(WorldButtonsPanel, Panel):
+    bl_label = "Materials and textures"
+    COMPAT_ENGINES = {'Panda3D'}
+
+    def draw(self, context):
+    
+        if hasattr(bpy.context.active_object.active_material, 'name'):
+            self.layout.template_preview(context.active_object.active_material)
+        
+        
+if __name__ == "__main__":
+    bpy.utils.register_module(__name__)
