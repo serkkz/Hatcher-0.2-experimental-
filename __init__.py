ff27b2b25adcd0d754dbb6ff6b4ef082479be556
Sergei Korotkov <serega-kkz@Mail.ru>
2016-05-05 10:03:55 +0300

    Added files via upload

--- __init__.py
+++ __init__.py
@@ -17,39 +17,62 @@ import shutil
 
 from .modules.panda3d_render import RenderPanda3D
 
-# Директория исходных файлов блендера
+# Собираем директорию исходных файлов блендера
 adress_bl_ui = os.path.join(os.getcwd(), str(bpy.app.version[0])+'.'+str(bpy.app.version[1]), 'scripts', 'startup',  'bl_ui')  
 
 # Собираем адрес директории для резервных копий исходных файлов блендера
 adress_dir_backup = os.path.join(os.getcwd(), str(bpy.app.version[0])+'.'+str(bpy.app.version[1]), 'scripts', 'startup', 'bl_ui', 'backup')
 
-# Aдрес файла исходника блендера properties_world.py
-properties_world = os.path.join(os.getcwd(), str(bpy.app.version[0])+'.'+str(bpy.app.version[1]), 'scripts', 'startup',  'bl_ui', 'properties_world.py')    
+# Собираем директорию модифицированых файлов расположеных в папке аддона
+dir_modified_files = os.path.join(os.path.dirname(__file__), 'modules', 'modified_files') 
+
 
 def register():
 
     # Проверяем на существование директории.
     if os.path.exists(adress_dir_backup):
-    
-        print('Directory backup exist')
+
+        print('')
+        print('Directory backup exist\n')
         
     else:
-
+    
+        print('')
+        print('Instail mod\n')
+        
         # Если её не оказалось то создаем
         os.makedirs(adress_dir_backup, mode=0o777, exist_ok=False)
-        print('Create a directory backup')
         
-        # Делаем копию файла properties_world.py и размещаем в директории резевного копирования.
-        shutil.copy(properties_world, adress_dir_backup)
-        print('properties_world.py copied to the backup directory')
-
-        # Собираем адрес файла расположеного в папке аддона которым подменим  файл блендера properties_world.py
-        properties_world_mod = os.path.join(os.path.dirname(__file__), 'modules', 'modified_files', 'properties_world.py')   
+        print('Create folder: backup')
+        print('Directory: '+ adress_bl_ui +'\n')
         
-        # Копируем properties_world.py и из папки адонна в блендер.
-        shutil.copy(properties_world_mod, adress_bl_ui)
-        print('Copy properties_world.py to the blender directory')
+        # Получаем список всех модифицированых файлов
+        modified_files = os.listdir(path=dir_modified_files)   
+        
+        # Проходим по списку извлекая имена
+        for name in modified_files:
+            
+            # Собираем полный адрес исходных файлов расположенных в папке bl_ui блендера
+            full_adress_sourse = os.path.join(adress_bl_ui, name)       
 
+            # Делаем копию файла и размещаем в директории backup.
+            shutil.copy(full_adress_sourse, adress_dir_backup)
+            
+            print('Copy file: ' + full_adress_sourse)
+            print('Directory: ' + adress_dir_backup + '\n')
+            
+            # Собираем полный адрес файлов расположенных в папке модифицированых файлов
+            full_adress_mod = os.path.join(dir_modified_files, name)
+            
+            # Делаем копию файла и размещаем в директории bl_ui.
+            shutil.copy(full_adress_mod, adress_bl_ui)
+            
+            print('Replace file: ' + full_adress_sourse)
+            print('File: ' + full_adress_mod + '\n')
+            
+
+        print('Instail finih\n')
+        
     bpy.utils.register_class(RenderPanda3D)
 
     from bl_ui import ( properties_render, properties_material,)
@@ -60,6 +83,10 @@ def unregister():
 
     # Проверяем на существование директории.
     if os.path.exists(adress_dir_backup):
+           
+        print('')
+        
+        print('Uninstail mod\n')
     
         # Получаем список всех сохраненых файлов
         list_file_restore = os.listdir(path=adress_dir_backup)
@@ -72,14 +99,18 @@ def unregister():
             
             # Делаем копию файла и размещаем в директории bl_ui.
             shutil.copy(full_adress, adress_bl_ui)
-            #print (list_file_restore[name]))
+            
+            print ('Restore file: ' + full_adress)
+            print ('Directory: ' + adress_bl_ui + '\n')
             
         shutil.rmtree(adress_dir_backup)
-        print ('Remove directory' + adress_bl_ui)
+
+        print ('Remove directory: ' + adress_dir_backup + '\n')
+        print('Uninstail finih\n')
         
     else:
     
-        print('No backup directory')
+        print('No backup directory\n' )
 
     bpy.utils.unregister_class(RenderPanda3D)
 
